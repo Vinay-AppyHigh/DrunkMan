aca9b9a15035633b0b3133b59340116186a8b744
baddka <itsvinaydude@gmail.com>
2020-12-08 19:02:45 +0530

    New Obstacles Added

---
+++ Assets/Scripts/MoveLivingsForward.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLivingsForward : MonoBehaviour
+{
+    public float Movespeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-transform.forward * Movespeed);
+    }
+}
