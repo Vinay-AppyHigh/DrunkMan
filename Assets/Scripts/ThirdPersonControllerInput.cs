61597c8d9b199895ac66d6e27c043d75cb524baa
baddka <itsvinaydude@gmail.com>
2021-01-26 11:35:57 +0530

    New Controls Added

---
+++ Assets/Scripts/ThirdPersonControllerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class ThirdPersonControllerInput : MonoBehaviour
+{
+    public FloatingJoystick Joystick;
+
+    public ThirdPersonUserControl Control;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Control = GetComponent<ThirdPersonUserControl>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Control.Hinput = Joystick.Horizontal;
+        Control.Vinput = 1; //Joystick.Vertical;
+    }
+}
